a9ef69333827da58640f438ab3a716933b001bbf
Kimmby <36679140+Kimmby@users.noreply.github.com>
2018-02-20 16:02:50 -0500

    Update comboString.java

--- java/string-1/comboString.java
+++ java/string-1/comboString.java
@@ -1,14 +1,8 @@
-/* Given 2 strings, a and b, return a string of the form short+long+short, 
- * with the shorter string on the outside and the longer string on the 
- * inside. The strings will not be the same length, but they may be empty 
- * (length 0).
- */
-public String comboString(String a, String b) {
-    if(b.length() < a.length()) {
-        String temp = a;
-        a = b;
-        b = temp;
-    }
-                  
-    return a + b + a;
+public String comboString(String a, String b) { //Given string a and b.
+  if (a.length() < b.length()){ //If string a is smaller than string b 
+    return a + b + a;           //then output the smaller numbers on the outside.
+  }
+  else{                         // On the other hand if string b is bigger than string a
+    return b + a + b;           // output b.
+  }
 }
